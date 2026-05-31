4522506eba0059a1b5beb40f5ebaa1dce1e18247
Areaboy19 <robertotriso239@gmail.com>
2026-05-31 22:33:01 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Robert Bortey Otriso
+# STUDENT ID:  2425404097
+# Email: 2425404097@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Robert Bortey Otriso, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials RBO in stylized large block letters using their respective characters
+
+    print()
+    print("...RBO")
+    print()
+    print("...RRRRRRRR...........BBBBBBBB...............OOOO...........")
+    print("...RR......RR.........BB......BB...........OO....OO.........")
+    print("...RR......RR.........BB......BB..........OO......OO........")
+    print("...RRRRRRRR...........BBBBBBBB...........OO........OO.......")
+    print("...RRRR...............BB......BB.........OO........OO.......")
+    print("...RR.RR..............BB......BB.........OO........OO.......")
+    print("...RR..RR.............BB......BB.........OO........OO.......")
+    print("...RR...RR............BB......BB..........OO......OO........")
+    print("...RR....RR....****...BB......BB..****.....OO....OO..****...")
+    print("...RR.....RR...****...BBBBBBBB....****.......OOOO....****...")
+    print()
+
+
+main()
